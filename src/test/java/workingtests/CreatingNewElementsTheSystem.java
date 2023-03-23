d6fb4995f648a4f8f3a9047a8de2178c67ba4e30
CherArtur <cherepanovartur@gmail.com>
2023-03-23 18:46:09 +0500

    changes in moveAdministrationSectionAddNewUser

--- src/test/java/workingtests/CreatingNewElementsTheSystem.java
+++ src/test/java/workingtests/CreatingNewElementsTheSystem.java
@@ -20,7 +20,11 @@ public class CreatingNewElementsTheSystem extends BaseConfig {
             userPassword = "admin",
             nameBriefcase = "Тестовый портфель",
             nameProject = "Первый проект",
-            nameTask = "Новая_задача";
+            nameTask = "Новая_задача",
+            nameOrganization = "Тест_организация",
+            nameMemberOrganization = "Стажер_техник",
+            loginMemberOrganization = "TraineeUser",
+            passwordMemberOrganization = "pass";
 
 
     @Tags({@Tag("HIGHEST"), @Tag("UI_TEST")})
@@ -102,7 +106,7 @@ public class CreatingNewElementsTheSystem extends BaseConfig {
     @Tags({@Tag("LOW"), @Tag("UI_TEST")})
     @DisplayName("Перейти в раздел Сотрудники и открыть карточку пользователя Администратор")
     @Test
-    void moveEmployeesSection() {
+    void moveEmployeesSectionOpenCardUser() {
 
         open(baseUrl);
         $(".auth-page__login-form").shouldHave(text("Вход"));
@@ -112,12 +116,60 @@ public class CreatingNewElementsTheSystem extends BaseConfig {
         $("nav").$(byText("Сотрудники")).click();
         $(".share-layout__header").shouldHave(text("Сотрудники"));
         $(".table-body").$(byText("Администратор")).click();
+        $(".profile-header__title").shouldHave(text("Администратор"), Duration.ofSeconds(5));
+        $("app-history-back-button").shouldBe(enabled).click();
+    }
+
+    @Tags({@Tag("HIGH"), @Tag("UI_TEST")})
+    @DisplayName("Перейти в раздел Администрирование и создать новую организацию")
+    @Test
+    void moveAdministrationSectionAddNewOrganization() {
+
+        open(baseUrl);
+        $(".auth-page__login-form").shouldHave(text("Вход"));
+        $("[name=username]").setValue(userLogin);
+        $("[name=password]").setValue(userPassword).pressEnter();
+        $("app-header").shouldHave(text("Проекты"), Duration.ofSeconds(5));
+        $("nav").$(byText("Администрирование")).click();
+        $(".share-layout__header").shouldHave(text("Администрирование"));
+        $(".share-layout__header-center").$(byText("Организации")).click();
+        $("[label=Добавить]").click();
+        $("[role=dialog]").shouldBe(visible).shouldHave(text("Создание организации"));
+        $("[id=name]").setValue(nameOrganization);
+        $("[label=Сохранить]").click();
+        $(".table_simple").shouldHave(text(nameOrganization));
+
+    }
+
+    @Tags({@Tag("HIGH"), @Tag("UI_TEST")})
+    @DisplayName("Перейти в раздел Администрирование и создать нового пользователя")
+    @Test
+    void moveAdministrationSectionAddNewUser() {
+
+        open(baseUrl);
+        $(".auth-page__login-form").shouldHave(text("Вход"));
+        $("[name=username]").setValue(userLogin);
+        $("[name=password]").setValue(userPassword).pressEnter();
+        $("app-header").shouldHave(text("Проекты"), Duration.ofSeconds(5));
+        $("nav").$(byText("Администрирование")).click();
+        $(".share-layout__header").shouldHave(text("Администрирование"));
+        $(".share-layout__header").$(byText("Добавить")).click();
+        $("[role=dialog]").shouldBe(visible).shouldHave(text("Создание пользователя"));
+        $(".name").setValue(nameMemberOrganization);
+        $("#login").setValue(loginMemberOrganization);
+        $("#pass").setValue(passwordMemberOrganization);
+        $("#organization").click();
+        $(".scrollable-content").$(byText(nameOrganization)).click();
+        $(".admin-users-roles").click();
+        $(".scrollable-content").$(byText("Исполнитель")).click();
+        $("[label=Сохранить]").click();
+
     }
 
     @Tags({@Tag("MEDIUM"), @Tag("UI_TEST")})
-    @DisplayName("Перейти в раздел Администрирование")
+    @DisplayName("В боковой карточке задачи добавить нового пользователя в участники задачи")
     @Test
-    void moveAdministrationSection() {
+    void addNewUserForTaskMembers() {
 
         open(baseUrl);
         $(".auth-page__login-form").shouldHave(text("Вход"));
